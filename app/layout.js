2832b576423ebe4b314f8fbb1a978cd50f1ef142
riccardocero2008-boop <riccardocero2008@gmail.com>
2026-07-04 16:31:26 +0200

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,4 @@
+import Script from "next/script";
 import { Analytics } from '@vercel/analytics/next';
 
 export const metadata = {
@@ -16,6 +17,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body style={{ margin: 0, padding: 0, overflow: 'hidden' }}>
+  <Script
+  src="https://quge5.com/88/tag.min.js"
+  data-zone="256282"
+  data-cfasync="false"
+  strategy="afterInteractive"
+/>
         {children}
         <Analytics />
       </body>
